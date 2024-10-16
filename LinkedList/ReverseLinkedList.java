da97400fa174c49a11140c48415950cd754cfeb9
Harit Patel <haritpatel789@gmail.com>
2024-10-16 22:08:11 +0530

    initial commit

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+public class ReverseLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+}
